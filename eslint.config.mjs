49a890cf55cb90a5f1d33171af25387f944d31ba
ziho7 <78910421@qq.com>
2025-03-05 21:37:57 +0800

    feat add rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
       "prefer-const": "off"
     }
   }
